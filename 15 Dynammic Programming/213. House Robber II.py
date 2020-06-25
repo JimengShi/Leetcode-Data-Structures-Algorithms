3591144e9d3d2d6e1d4bafda8848fe129a4032cf
Jimeng Shi <60025043+JimengShi@users.noreply.github.com>
2020-06-24 20:06:55 -0400

    Add files via upload

--- 15 Dynammic Programming/213. House Robber II.py
+++ 15 Dynammic Programming/213. House Robber II.py
@@ -1,32 +1,43 @@
-## APPROACH : DP ##
-## LOGIC ##
-## 1. Only 2 scenarios possible 
-##     a) Rob 1st and donot rob last 
-##     b) Rob last and donot rob first. 
-## We take maximum of both cases.
+# Only 2 scenarios possible, We take maximum of both cases.
+# a) Rob 1st and donot rob last 
+# b) Rob last and donot rob first. 
 
-
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        def house_robber(nums):
-            dp = [0] * len(nums)
-            dp[0] = nums[0]
-            dp[1] = max(nums[0], nums[1])
-            for i in range(2, len(nums)):
-                dp[i] = max(dp[i-1], nums[i]+dp[i-2])
-            return max(dp[-1], dp[-2])
-        
-        if len(nums) <= 2: 
-            return max([0] + nums)
-        return max( house_robber(nums[1:]), house_robber(nums[:-1]) )  
-
-# if robbing 1st house
 #                 rob 1     not rob 1    
 # f(1,n) = max(  f(1,n-1),   f(2,n)   )
 
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # (0) edge case
+        if len(nums) <= 2:
+            return max([0] + nums)
+        
+        # (1) function for problem 198
+        def house_robber(arr):
+            n = len(arr)
+            if n == 0:
+                return 0
+            if n == 1:
+                return arr[0]
+
+            dp = [ [0] * 2 for _ in range(n+1)]
+            for i in range(1, n+1):
+                dp[i][0] = max(dp[i-1][0], dp[i-1][1])  # forget it
+                dp[i][1] = arr[i-1] + dp[i-1][0]        # let's do it
+            return max(dp[n][0], dp[n][1]) 
+        
+        # (2) return Rob 1st and donot rob last and Rob last and donot rob first.
+        return max( house_robber(nums[1:]), house_robber(nums[:-1]) )  
+
+# Time: O(n)
+# Space: O(n)
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 2: 
+            return max([0] + nums)
+        
         def house_robber(nums):
             n = len(nums)
             if n == 0:
@@ -38,6 +49,8 @@ class Solution:
                 no, yes = max(no, yes), i + no
             return max(no, yes)
         
-        if len(nums) <= 2: 
-            return max([0] + nums)
+
         return max( house_robber(nums[1:]), house_robber(nums[:-1]) )
+    
+# Time: O(n)
+# Space: O(1)
