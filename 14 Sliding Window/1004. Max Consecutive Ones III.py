56b26beb0c0179b1cc38306947889046b6b253be
Jimeng Shi <60025043+JimengShi@users.noreply.github.com>
2020-06-29 00:27:03 -0400

    Add files via upload

--- 14 Sliding Window/1004. Max Consecutive Ones III.py
+++ 14 Sliding Window/1004. Max Consecutive Ones III.py
@@ -1,14 +1,35 @@
-# Input: A = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], K = 2
-# Output: 6, [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1]
+# Method 1:
+class Solution:
+    def longestOnes(self, A: List[int], K: int) -> int:
+        # (0) edge case
+        if not A:
+            return 0
+        
+        # (1) initialize two points and count
+        l = 0
+        res = 0
+        count = 0
+        
+        # (2) traverse
+        for r in range(len(A)):
+            if A[r] == 0:          # (2.1) count number of 0
+                count += 1
+                
+            while count > K:       # (2.2) shrink window when count > K
+                if A[l] == 0:
+                    count -= 1
+                l += 1
+                
+            res = max(res, r-l+1)  # (2.3) update result
+        
+        # (3) return result
+        return res
     
-# 这道题和通用套路不同的是，我们只需要记录下加入窗口的是0还是1：
-# 如果是1，我们什么都不用做
-# 如果是0，我们将K减1
+# Time: O(N), visit every element of array twice, once by left pointer and once by right pointer.
+# Space: O(1), We do not use any extra space.
+    
 
-# 相应地，我们需要记录移除窗口的是0还是1:
-# 如果是1，我们什么都不做
-# 如果是0，说明加进来的时候就是1，加进来的时候我们K减去了1，这个时候我们再加1。
-
+# Method 2:
 class Solution:
     def longestOnes(self, A: List[int], K: int) -> int:
         # (1) initialze left and right pointers and result = 0
@@ -17,7 +38,7 @@ class Solution:
         # (2) traverse the list
         for r in range(len(A)):
             K -= 1 - A[r]                   # (2.1) K = K-1 if A[r] == 0; K will change nothing if A[r] == 1
-            if K < 0:                       # (2.2) enter K < 0 --> we will more left pointer ahead and update K
+            if K < 0:                       # (2.2) enter K < 0 --> move left pointer ahead and update K
                 while l < r and A[l] == 1:
                     l += 1
                 l += 1
@@ -27,5 +48,6 @@ class Solution:
         # (3) return result
         return res 
     
-# Time: O(N), we might end up visiting every element of array twice, once by left pointer and once by right pointer.
+    
+# Time: O(N), visit every element of array twice, once by left pointer and once by right pointer.
 # Space: O(1), We do not use any extra space.
