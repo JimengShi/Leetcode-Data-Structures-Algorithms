26904239679e7654ee2def1d636479532028b62d
Jimeng Shi <60025043+JimengShi@users.noreply.github.com>
2020-09-10 20:23:04 -0400

    Add files via upload

--- 14 Sliding Window/209. Minimum Size Subarray Sum.py
+++ 14 Sliding Window/209. Minimum Size Subarray Sum.py
@@ -1,3 +1,14 @@
+# Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+
+# Example: 
+# Input: s = 7, nums = [2,3,1,2,4,3]
+# Output: 2
+# Explanation: the subarray [4,3] has the minimal length under the problem constraint.
+
+# Follow up:
+# If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n). 
+
+
 # Method 1: Brute Force
 class Solution:
     def minSubArrayLen(self, s: int, nums: List[int]) -> int:
@@ -5,16 +16,17 @@ class Solution:
             return 0
         
         n = len(nums)
-        ans = n + 1
+        min_len = n + 1
+
         for i in range(n):
-            total = 0
+            cur_sum = 0
             for j in range(i, n):
-                total += nums[j]
-                if total >= s:
-                    ans = min(ans, j - i + 1)
+                cur_sum += nums[j]
+                if cur_sum >= s:
+                    min_len = min(min_len, j-i+1)
                     break
         
-        return 0 if ans == n + 1 else ans
+        return 0 if min_len == n + 1 else min_len
 
 # Time: O(N^2)
 # Space: O(1)
@@ -29,11 +41,11 @@ class Solution:
         n = len(nums)
         ans = n + 1
         sums = [0]                                    # sums[i]: nums[0] 到 nums[i−1] 的元素和, 共 i 个元素
-        for i in range(n):
+        for i in range(n):                            # nums = [2, 3, 1, 2, 4, 3]
             sums.append(sums[-1] + nums[i])           # sums = [0, 2, 5, 6, 8, 12, 15]
         
-        for i in range(1, n + 1):
-            target = s + sums[i - 1]
+        for i in range(1, n+1):
+            target = s + sums[i-1]
             bound = bisect.bisect_left(sums, target)
             if bound != len(sums):
                 ans = min(ans, bound - i + 1)
--- 14 Sliding Window/480. Sliding Window Median.py
+++ 14 Sliding Window/480. Sliding Window Median.py
@@ -25,32 +25,3 @@
     
 # Time: O((n-k+1)·k·logk): O(n-k) for traverse the nums and O(klogk) for sorting
 # Space: O(n−k+1) extra linear space for the window container.
-
-
-
-# Method 2: 
-class Solution(object):
-    def medianSlidingWindow(self, nums, k):
-        res, window = [], []
-        
-        for i in range(len(nums)):
-            # Find position where outgoing element should be removed from
-            if i >= k:
-                # window.remove(nums[i-k])
-                window.pop(bisect.bisect(window, nums[i-k]) - 1)
-
-            # Maintain the sorted invariant while inserting incoming element
-            bisect.insort(window, nums[i])
-
-            # Find the medians
-            if i >= k - 1:
-                if k % 2 == 1:
-                    median = float(window[k // 2])    
-                else:
-                    median = float((window[k//2 - 1] + window[k//2]) * 0.5)
-                res.append(median)
-
-        return res
-    
-# Time: O((n-k+1)·(k+logk)): O(n-k+1) is the number of window O(logk) for bisect.bisect, O(k) for bisect.insort
-# Space: O(n−k+1) for result, O(k) for window container.
