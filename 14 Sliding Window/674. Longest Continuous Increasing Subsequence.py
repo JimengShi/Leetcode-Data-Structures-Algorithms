f46bc5f6a0381a79b2c32db663453162a64abe60
Jimeng Shi <60025043+JimengShi@users.noreply.github.com>
2021-09-12 10:04:42 -0400

    Update 674. Longest Continuous Increasing Subsequence.py

--- 14 Sliding Window/674. Longest Continuous Increasing Subsequence.py
+++ 14 Sliding Window/674. Longest Continuous Increasing Subsequence.py
@@ -1,13 +1,38 @@
 class Solution:
     def findLengthOfLCIS(self, nums: List[int]) -> int:
-        result, count = 0, 0
-        for i in range(len(nums)):
-            if i == 0 or nums[i-1] < nums[i]:
-                count += 1
-                result = max(result, count)
+        temp_count = 1
+        max_count = 1
+        
+        for i in range(len(nums)-1):
+            if nums[i] < nums[i+1]:
+                temp_count += 1
+                max_count = max(max_count, temp_count)
             else:
-                count = 1
-        return result
+                temp_count = 1
+                
+        return max_count
+
+    
+
+# SIMILAR question: find the length of the longest substring with the repeating letter 
+
+# class Solution:
+#     def findLengthOfLongestSubstring(self, s):    
+#         cur_count = 1
+#         max_count = 0
+
+#         for i in range(len(s)-1):
+#             if s[i] == s[i+1]:
+#                 cur_count += 1
+#                 max_count = max(max_count, cur_count)
+#             else:
+#                 cur_count = 1
+
+#         return max_count
+
+    
+# # s = 'ABBBBBABA'
+# # s = '101101111'
     
 # Time: O(N), where N is the length of nums. We perform one loop through nums.
 # Space: O(1), the space used by result and count.
