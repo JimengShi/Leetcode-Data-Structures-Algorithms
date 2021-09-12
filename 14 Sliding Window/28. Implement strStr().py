c8eec2cf357350ee163ff3a969f3834ea5b92524
Jimeng Shi <60025043+JimengShi@users.noreply.github.com>
2021-09-12 11:29:06 -0400

    Update 28. Implement strStr().py

--- 14 Sliding Window/28. Implement strStr().py
+++ 14 Sliding Window/28. Implement strStr().py
@@ -1,15 +1,17 @@
 # Method 1: Slice
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        L, n = len(needle), len(haystack)
+        l1, l2 = len(needle), len(haystack)
 
-        for start in range(n - L + 1):
-            if haystack[start: start + L] == needle:
-                return start
+        for i in range(l1-l2+1):
+            if haystack[i:i+l2] == needle:
+                return i
+                break
         return -1
     
 # Time: O((N−L)L), where N is a length of haystack and L is a length of needle. We compute a substring of length L in a loop, which is executed (N - L) times.
 # Space: O(1).
+
 
 
 # Method 2: sliding window
@@ -20,14 +22,15 @@ class Solution:
         if l1 < l2: 
             return -1
         
-        # (1) traverse array for moving window
-        for i in range(l1 - l2 + 1):
-            # find start index and count
-            count = 0
-            while count < l2 and haystack[i + count] == needle[count]: 
-                count += 1
-            if count == l2:
-                return i
+        # (1) traverse array for moving window, i is used for traversing haystack, j is used for traversing needle.
+        for i in range(l1-l2+1):
+            temp_index = i
+            j = 0
+            while i < l1 and j < l2 and haystack[i]==needle[j]: 
+                i += 1
+                j += 1
+            if j == l2:
+                return temp_index
             
         # (2) otherwise return -1
         return -1
